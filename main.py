5c60c380919c7aef17b75bdbbe9b81d0a452f807
ChenXu <chenxuhuang27@gmail.com>
2026-05-18 19:23:33 +0800

    fix(hotfix): add macOS Nuitka compilation parameters

--- main.py
+++ main.py
@@ -2,16 +2,30 @@
 
 # nuitka-project-if: {OS} == "Darwin":
 #    nuitka-project: --mode=app
-# nuitka-project-else:
-#    nuitka-project: --mode=standalone
+#    nuitka-project: --macos-signed-app-name=com.chenxu-huang.courier
+#    nuitka-project: --macos-sign-identity=ad-hoc
+#    nuitka-project: --macos-app-mode=ui-element
+#    nuitka-project: --macos-app-name=Courier
+#    nuitka-project: --macos-app-icon=none
 
-# nuitka-project: --disable-console
+# nuitka-project-if: {OS} == "Windows":
+#    nuitka-project: --mode=standalone
+#    nuitka-project: --windows-console-mode=disable
+# --windows-icon-from-ico=resources/icon.ico
+
+# nuitka-project-if: {OS} in ("Linux", "FreeBSD"):
+#    nuitka-project: --mode=standalone
+# --linux-icon=resources/icon.png
+
+# nuitka-project: --deployment
 # nuitka-project: --assume-yes-for-downloads
 # nuitka-project: --enable-plugin=pyside6
 # nuitka-project: --output-dir=dist
 # nuitka-project: --output-filename=Courier
 # nuitka-project: --product-name=Courier
 # nuitka-project: --include-distribution-metadata=courier
+# nuitka-project: --noinclude-unittest-mode=error
+# nuitka-project: --noinclude-pytest-mode=error
 
 import sys
 
